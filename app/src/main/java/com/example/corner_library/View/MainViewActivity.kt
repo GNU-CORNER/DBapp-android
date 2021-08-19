57700b94b0a6f0a80148c503427846fe3dc84d0c
markyul <pig052656@naver.com>
2021-08-19 20:09:03 +0900

    View 파일 생성

---
+++ app/src/main/java/com/example/corner_library/View/MainViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.corner_library.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.corner_library.R
+
+class MainViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_view)
+    }
+}
