27d38cbd385ad01cadc987397a7594211dde7ee6
jaeseuk <sjs4126@naver.com>
2021-12-07 16:12:56 +0900

    SearchResultActivity 수정
    SearchResultActivity.kt
    -InputMethodManager 액티비티 멤버 변수로 이동
    -viewModel 타입 SearchResultViewModel로 수정 및 ViewModelFactory를 사용한 초기화 추가
    -키보드 숨김 메소드 hideKeyboard 추가
    -Toast 메시지 및 주석 처리 코드 제거
    -자동완성 검색어 리사이클러뷰 설정 코드 추가
    -검색 EditText 설정 코드 메소드화 및 doOnTextChanged 리스너 추가

--- app/src/main/java/com/example/corner_library/view/activity/SearchResultActivity.kt
+++ app/src/main/java/com/example/corner_library/view/activity/SearchResultActivity.kt
@@ -1,71 +1,70 @@
 package com.example.corner_library.view.activity
 
 import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
-import androidx.activity.viewModels
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.corner_library.R
 import com.example.corner_library.adapters.MiniProjectAdapter
+import com.example.corner_library.adapters.SearchSuggestionAdapter
+import com.example.corner_library.data.model.Category
 import com.example.corner_library.databinding.ActivitySearchResultBinding
-import com.example.corner_library.viewmodel.MainViewModel
+import com.example.corner_library.viewmodel.SearchResultViewModel
+import com.example.corner_library.viewmodel.ViewModelFactory
 
 class SearchResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchResultBinding
-    private val viewModel: MainViewModel by viewModels()
+    private lateinit var viewModel: SearchResultViewModel
+    private val inputMethodManager by lazy {
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
 
+        viewModel = ViewModelProvider(
+            this, ViewModelFactory(
+                intent.getStringExtra("query"),
+                intent.getParcelableExtra<Category>("Category")
+            )
+        ).get(SearchResultViewModel::class.java)
+
         setBinding()
         setRecyclerView()
-
+        setSearchEditText()
 
         binding.toolbar.setNavigationIcon(R.drawable.back_icon)
 
-//        initData()
-
-        binding.toolbar.setNavigationOnClickListener(View.OnClickListener {
+        binding.toolbar.setNavigationOnClickListener {
             finish()
-        })
-
-        // 검색 입력창 관련 메소드
-        binding.searchEditText.setOnKeyListener { view, keyCode, keyEvent ->
-            var handled = false
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-            // 엔터 또는 키보드 내 완료 버튼 눌렀을때 행동
-            if (keyEvent.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                imm.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
-                val intent = Intent(this, SearchResultActivity::class.java)
-                finish()
-                startActivity(intent)
-//                Toast.makeText(this, "검색 입력 완료", Toast.LENGTH_SHORT).show()
-            }
-
-            handled
         }
 
-        binding.dropdownSort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                when (binding.dropdownSort.getItemAtPosition(position)){
+        binding.dropdownSort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (binding.dropdownSort.getItemAtPosition(position)) {
                     "최신순" -> {
-                        Toast.makeText(parent?.context, "최신순", Toast.LENGTH_SHORT).show()
+
                     }
                     "오래된순" -> {
-                        Toast.makeText(parent?.context, "오래된순", Toast.LENGTH_SHORT).show()
+
                     }
                     "오름차순" -> {
-                        Toast.makeText(parent?.context, "오름차순", Toast.LENGTH_SHORT).show()
+
                     }
                     "내림차순" -> {
-                        Toast.makeText(parent?.context, "내림차순", Toast.LENGTH_SHORT).show()
+
                     }
                 }
             }
@@ -76,12 +75,9 @@ class SearchResultActivity : AppCompatActivity() {
         }
     }
 
-
-
     private fun setBinding() {
         binding = ActivitySearchResultBinding.inflate(layoutInflater)
         binding.lifecycleOwner = this
-        binding.view = this
         binding.viewModel = viewModel
         setContentView(binding.root)
     }
@@ -93,17 +89,41 @@ class SearchResultActivity : AppCompatActivity() {
             adapter = MiniProjectAdapter("search")
             setHasFixedSize(true)
         }
+
+        binding.rvSuggestions.apply {
+            layoutManager =
+                LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            adapter =
+                SearchSuggestionAdapter("SearchResultActivity") { query ->
+                    viewModel.searchProject(query)
+                    hideKeyboard()
+                }
+        }
     }
 
-//    private fun initData() {
-//
-//        projects.add(Project(name = "신선고", subject = "냉장고 관리", logo = R.drawable.logo2))
-//        projects.add(Project(name = "전인혁", subject = "인혁이 관리", logo = R.drawable.logo))
-//        projects.add(Project(name = "Local Sharing", subject = "밥집 관리", logo = R.drawable.logo3))
-//        projects.add(Project(name = "신선고", subject = "냉장고 관리", logo = R.drawable.logo2))
-//        projects.add(Project(name = "앱앱고", subject = "앱 관리", logo = R.drawable.logo))
-//        projects.add(Project(name = "Local Sharing", subject = "밥집 관리", logo = R.drawable.logo3))
-//        projects.add(Project(name = "이거시 뭐시여", subject = "몰라 관리", logo = R.drawable.logo2))
-//
-//    }
+    private fun setSearchEditText() {
+        // 검색 입력창 관련 메소드
+        binding.searchEditText.apply {
+            setOnEditorActionListener { view, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {   // 키보드의 검색 버튼 클릭 시
+                    if (view.text.isNotEmpty()) viewModel.searchProject(view.text.toString())
+                    hideKeyboard()
+
+                    return@setOnEditorActionListener true
+                }
+
+                return@setOnEditorActionListener false
+            }
+            // 텍스트 입력 변경 시
+            doOnTextChanged { query, _, _, _ ->
+                query?.let { viewModel.searchSuggestion(query.toString()) }
+            }
+        }
+    }
+
+    // 키보드 숨김
+    private fun hideKeyboard() {
+        inputMethodManager.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
+        binding.searchEditText.clearFocus()
+    }
 }
